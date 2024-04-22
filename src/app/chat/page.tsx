3e8763565e34f88032f33b8690f3846f245d7803
Aditya Apte <adityaapte2014@gmail.com>
2024-04-22 19:15:39 +0530

    Final Commit

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -4,12 +4,13 @@ import { useChat } from 'ai/react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { ArrowUp } from 'lucide-react';
-import React, { use, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Image from 'next/image';
 import logo from '../../../public/logo.svg';
 import { UserAvatar } from '@/components/ui/user-avatar';
 import { cn } from '@/lib/utils';
 import { AIAvatar } from '@/components/ui/ai-avatar';
+import { currentUser } from '@clerk/nextjs/server';
 
 export default function Chat() {
     const { messages, input, handleInputChange, handleSubmit } = useChat();
@@ -50,7 +51,7 @@ export default function Chat() {
                             <Image src={logo} alt='IntelliCompanion Logo' className='w-[3%] h-[3%]'></Image>
                         </div>
                         <div className='w-full flex justify-center mt-4'>
-                            <h1 className='font-bold text-xl'>
+                            <h1 className='font-bold text-3xl'>
                                 How can I help you today?
                             </h1>
                         </div>
